3b25770cff79dbdffc7bb0007490691a98f8002e
Jeff Dederick <Xpressivmotion@gmail.com>
2018-03-28 14:07:36 -0400

    errors out

--- Navigation_and_Routing/Component.js
+++ Navigation_and_Routing/Component.js
@@ -1,14 +1,15 @@
-sap.ui.define([
-	"sap/ui/core/UIComponent"
-], function (UIComponent) {
-	"use strict";
+sap.ui.define(['sap/ui/core/UIComponent'], function(UIComponent) {
+  'use strict';
 
-	return UIComponent.extend("sap.ui.demo.nav.Component", {
+  return UIComponent.extend('sap.ui.demo.nav.Component', {
+    metadata: {
+      manifest: 'json'
+    },
 
-		metadata: {
-			manifest: "json"
-		}
+    init: function() {
+      UIComponent.prototype.init.apply(this, arguments);
 
-	});
-
+      this.getRouter().initialize();
+    }
+  });
 });
