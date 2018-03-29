44422997a000d296189dda6518678bc6baac5ca8
Jeff Dederick <Xpressivmotion@gmail.com>
2018-03-29 10:56:03 -0400

    tutorial complete

--- Navigation_and_Routing/controller/Home.controller.js
+++ Navigation_and_Routing/controller/Home.controller.js
@@ -1,5 +1,22 @@
-sap.ui.define(['sap/ui/core/mvc/Controller'], function(Controller) {
+sap.ui.define(['sap/ui/demo/nav/controller/BaseController'], function(
+  BaseController
+) {
   'use strict';
 
-  return Controller.extend('sap.ui.demo.nav.controller.Home', {});
+  return BaseController.extend('sap.ui.demo.nav.controller.Home', {
+    onDisplayNotFound: function(oEvent) {
+      //display the "notFound" target without changing the hash
+      this.getRouter()
+        .getTargets()
+        .display('notFound', {
+          fromTarget: 'home'
+        });
+    },
+    onNavToEmployees: function(oEvent) {
+      this.getRouter().navTo('employeeList');
+    },
+    onNavToEmployeeOverview: function(oEvent) {
+      this.getRouter().navTo('employeeOverview');
+    }
+  });
 });
--- Navigation_and_Routing/localService/mockserver.js
+++ Navigation_and_Routing/localService/mockserver.js
@@ -1,38 +1,41 @@
-sap.ui.define([
-	"sap/ui/core/util/MockServer"
-], function (MockServer) {
-	"use strict";
-	var _sAppModulePath = "sap/ui/demo/nav/",
-		_sJsonFilesModulePath = _sAppModulePath + "localService/mockdata";
+sap.ui.define(['sap/ui/core/util/MockServer'], function(MockServer) {
+  'use strict';
+  var _sAppModulePath = 'sap/ui/demo/nav/',
+    _sJsonFilesModulePath = _sAppModulePath + 'localService/mockdata';
 
-	return {
+  return {
+    init: function() {
+      var sManifestUrl = jQuery.sap.getModulePath(
+          _sAppModulePath + 'manifest',
+          '.json'
+        ),
+        sJsonFilesUrl = jQuery.sap.getModulePath(_sJsonFilesModulePath),
+        oManifest = jQuery.sap.syncGetJSON(sManifestUrl).data,
+        oMainDataSource = oManifest['sap.app'].dataSources.employeeRemote,
+        sMetadataUrl = jQuery.sap.getModulePath(
+          _sAppModulePath +
+            oMainDataSource.settings.localUri.replace('.xml', ''),
+          '.xml'
+        );
 
-		init: function () {
-			var sManifestUrl = jQuery.sap.getModulePath(_sAppModulePath + "manifest", ".json"),
-				sJsonFilesUrl = jQuery.sap.getModulePath(_sJsonFilesModulePath),
-				oManifest = jQuery.sap.syncGetJSON(sManifestUrl).data,
-				oMainDataSource = oManifest["sap.app"].dataSources.employeeRemote,
-				sMetadataUrl = jQuery.sap.getModulePath(_sAppModulePath + oMainDataSource.settings.localUri.replace(".xml", ""), ".xml");
+      // create
+      var oMockServer = new MockServer({
+        rootUri: oMainDataSource.uri
+      });
 
-			// create
-			var oMockServer = new MockServer({
-				rootUri: oMainDataSource.uri
-			});
+      // configure
+      MockServer.config({
+        autoRespond: true,
+        autoRespondAfter: 1000
+      });
 
-			// configure
-			MockServer.config({
-				autoRespond: true,
-				autoRespondAfter: 1000
-			});
+      // simulate
+      oMockServer.simulate(sMetadataUrl, {
+        sMockdataBaseUrl: sJsonFilesUrl
+      });
 
-			// simulate
-			oMockServer.simulate(sMetadataUrl, {
-				sMockdataBaseUrl : sJsonFilesUrl
-			});
-
-			// start
-			oMockServer.start();
-		}
-	};
-
+      // start
+      oMockServer.start();
+    }
+  };
 });
